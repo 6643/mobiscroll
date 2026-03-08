dfd8d473db8411825710ad006f3cb5f5f78e8162
no if <191199666@qq.com>
2026-03-08 13:34:39 +0800

    Update trace.go

--- trace.go
+++ trace.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -21,7 +22,36 @@ func main() {
 	ip := parseIp(string(body))
 	fmt.Println("Parsed IP:", ip)
 
+	if ip != "" {
+		f, err := os.OpenFile("ip.text", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err == nil {
+			defer f.Close()
+			f.WriteString(ip + "\n")
+		} else {
+			fmt.Println("Failed to open ip.text:", err)
+		}
+	}
+
 }
+
+/*
+fl=80f417
+h=cloudflare.com
+ip=111.249.78.157
+ts=1772941360.629
+visit_scheme=https
+uag=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36
+colo=TPE
+sliver=none
+http=http/2
+loc=TW
+tls=TLSv1.3
+sni=plaintext
+warp=off
+gateway=off
+rbi=off
+kex=X25519MLKEM768
+*/
 
 func parseIp(str string) string {
 	for _, line := range strings.Split(str, "\n") {
